f32bc2d1dd855f662e387150558ed74e49695edd
LuisOliveira17 <gustavokreft@gmail.com>
2026-02-26 15:31:49 -0300

    Criacao classe de Conta

---
+++ Java- Fundamentos/AulasPoo/ProjetoConta/src/Conta.java
@@ -0,0 +1,22 @@
+public class Conta {
+    public String conta;
+    public String agencia;
+    public double saldo;
+    public String nomeCliente;
+
+    public int sacar(double valor){
+        return 0;
+    }
+
+    public void depositar(double valor){
+
+    }
+
+    public void imprimir(){
+
+    }
+
+
+}
+
+
