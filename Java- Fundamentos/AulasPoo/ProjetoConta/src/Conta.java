ff36e9be211eccd6ca169c4c8fce954d86b4e947
LuisOliveira17 <gustavokreft@gmail.com>
2026-02-26 15:56:34 -0300

    Instancia de metodo static

--- Java- Fundamentos/AulasPoo/ProjetoConta/src/Conta.java
+++ Java- Fundamentos/AulasPoo/ProjetoConta/src/Conta.java
@@ -4,17 +4,22 @@ public class Conta {
     public double saldo;
     public String nomeCliente;
 
-    public int sacar(double valor){
-        return 0;
+    public int sacar(double valorSaque){
+        if(saldo<valorSaque){
+            return 0;
+        }
+        return 1;
     }
 
-    public void depositar(double valor){
-
+    public void depositar(double valorRecebido){
+        saldo+=valorRecebido;
     }
 
     public void imprimir(){
-
-    }
+        System.out.println("conta = " + conta);
+        System.out.println("agencia = " + agencia);
+        System.out.println("saldo = " + saldo);
+        System.out.println("nomeCliente = " + nomeCliente);    }
 
 
 }
--- Java- Fundamentos/AulasPoo/ProjetoConta/src/ContaTeste.java
+++ Java- Fundamentos/AulasPoo/ProjetoConta/src/ContaTeste.java
@@ -1,2 +1,45 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class ContaTeste {
+    Conta cc = new Conta(); //Atributo publico da classe conta
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Menu:");
+        System.out.println("==================");
+        System.out.println("1.Cadastrar");
+        System.out.println("2.Depositar");
+        System.out.println("3.Sacar");
+        System.out.println("4.Consultar");
+        System.out.println("0.Sair");
+        System.out.println("==================");
+        System.out.println("Escolha:");
+
+        int opcao=Integer.parseInt(sc.nextLine()); //Limpar o Buffer;
+
+        switch (opcao){
+            case 1->{
+               execCadastrar();
+            }
+        }
+    }
+
+    //Quebrei a cabeça para decifrar o porque não chamava no switch;
+    //Como o main é um metodo static, nao chamará funcs. não estáticas
+    public static void execCadastrar(){
+
+    }
+
+    public void execConsultar(){
+
+    }
+
+    public void execSacar(){
+
+    }
+
+    public void execDepositar(){
+
+    }
 }
