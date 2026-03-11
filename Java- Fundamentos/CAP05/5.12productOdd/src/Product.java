a61486ae65ae271947eb35f71b321ca78dfe3ec4
LuisOliveira17 <gustavokreft@gmail.com>
2026-03-11 15:06:44 -0300

    Produto de impares

---
+++ Java- Fundamentos/CAP05/5.12productOdd/src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    public static void main(String[] args){
+        int produto=1;
+        for(int i=1;i<=15;i++){
+            if(i%2!=0){
+                System.out.println(i);
+                produto*=i;
+            }
+        }
+        System.out.println("Produto:"+produto);
+    }
+}
