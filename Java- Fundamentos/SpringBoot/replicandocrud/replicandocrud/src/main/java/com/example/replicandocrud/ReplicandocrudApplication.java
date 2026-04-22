d3c19b78aedd08cac6cf7b27437fdda6f335e3ff
LuisOliveira17 <gustavokreft@gmail.com>
2026-04-22 19:07:54 -0300

    Estudando SpringBoot

---
+++ Java- Fundamentos/SpringBoot/replicandocrud/replicandocrud/src/main/java/com/example/replicandocrud/ReplicandocrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.replicandocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicandocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicandocrudApplication.class, args);
+	}
+
+}
