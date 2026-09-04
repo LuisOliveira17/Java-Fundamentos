bd8ab9e063f1980619d4ca99c1758186b0f984ff
LuisOliveira17 <gustavokreft@gmail.com>
2026-09-04 01:05:50 -0300

    Aula de inicio sobre OPP

---
+++ Java- Fundamentos/Aulas BootCamp Itau/Java e a Arte da Abstracao - Classes e Encapsulamento/Exemplos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Person homem = new Person("nome",19);
+        Person mulher = new Person("Mariazinha",20);
+
+
+    }
+}
---
+++ Java- Fundamentos/Aulas BootCamp Itau/Java e a Arte da Abstracao - Classes e Encapsulamento/Exemplos/src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String nome;
+    private int age;
+
+    public Person(){
+
+    }
+
+    public Person(String nome,  int age){
+        this.nome = nome;
+        this.age = age;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
